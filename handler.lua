9f16e4ea57889a430f98d81fc9ae6009dae60cab
andy <andy.zhangtao@hotmail.com>
2018-04-08 10:00:34 +0800

    Fix strict url can not use as a cache key issue
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- handler.lua
+++ handler.lua
@@ -49,7 +49,7 @@ local function get_key(conf)
     local has_key = false
     if conf.key then
         ngx_log(NGX_ERR, conf.key)
-        cache_key = conf.key
+
         for s in string.gmatch(conf.key, '$([%w_-]+)') do
             has_key = true
             ngx_log(NGX_ERR, "Find key [" .. s .. "]")
@@ -60,6 +60,12 @@ local function get_key(conf)
                 cache_key = string.gsub(cache_key, "$" .. s, ngx.var[s])
             end
         end
+
+        if string.len(conf.key) >0 and not has_key then
+            cache_key = conf.key
+            has_key = true
+        end
+
     end
 
     if not has_key then
